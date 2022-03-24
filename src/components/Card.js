ff75baa82d2900f6c16b16c56bd804c531929d21
khalidfarooq <wandererkhalid@gmail.com>
2022-03-25 00:06:26 +0530

    List Card Input Component

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {Paper} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
+
+const useStyle = makeStyles((theme)=>({
+    card:{
+        padding: theme.spacing(1,1,1,2),
+        margin: theme.spacing(1),
+    }
+}));
+export default function Card(){
+    const classes = useStyle();
+    return(
+        <Paper className={classes.card}>coding for bharatpe</Paper>
+    );
+}
