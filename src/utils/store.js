d66cf35b0487745e3e1be488c8bef0fa0464b11f
khalid farooq <47777912+khalidfarooq@users.noreply.github.com>
2022-03-25 13:02:47 +0530

    Update store.js

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,15 +1,15 @@
 const cards = [
   {
     id: 'card-1',
-    title: 'Learning how to cook',
+    title: 'Make frontend responsive',
   },
   {
     id: 'card-2',
-    title: 'Making sandwich',
+    title: 'Do unit testing',
   },
   {
     id: 'card-3',
-    title: 'Taking the trash out',
+    title: 'Deploy the web application',
   },
 ];
 
